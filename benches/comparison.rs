af9b823cd4f77eaffd1d72b62aa87e3ec387488c
Dmitry Marakasov <amdmi3@amdmi3.ru>
2024-08-14 02:42:11 +0300

    Cargo fmt

--- benches/comparison.rs
+++ benches/comparison.rs
@@ -3,19 +3,23 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use libversion::*;
 
 fn comparison_benchmark(c: &mut Criterion) {
-    c.bench_function("compare numeric", |b| b.iter(|| {
-        version_compare2(
-            "1.2.3.4.5.6.7.8.9.10.11.12.13.14.15.16.17.18.19.20.21.22.23.24.25.26.27.28.29.30",
-            "1.2.3.4.5.6.7.8.9.10.11.12.13.14.15.16.17.18.19.20.21.22.23.24.25.26.27.28.29.30",
-        )
-    }));
+    c.bench_function("compare numeric", |b| {
+        b.iter(|| {
+            version_compare2(
+                "1.2.3.4.5.6.7.8.9.10.11.12.13.14.15.16.17.18.19.20.21.22.23.24.25.26.27.28.29.30",
+                "1.2.3.4.5.6.7.8.9.10.11.12.13.14.15.16.17.18.19.20.21.22.23.24.25.26.27.28.29.30",
+            )
+        })
+    });
 
-    c.bench_function("compare alphabetic short", |b| b.iter(|| {
-        version_compare2(
-            "a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z",
-            "a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z",
-        )
-    }));
+    c.bench_function("compare alphabetic short", |b| {
+        b.iter(|| {
+            version_compare2(
+                "a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z",
+                "a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z",
+            )
+        })
+    });
 
     c.bench_function("compare letter suffix", |b| b.iter(|| {
         version_compare2(
@@ -31,19 +35,23 @@ fn comparison_benchmark(c: &mut Criterion) {
         )
     }));
 
-    c.bench_function("compare mixed", |b| b.iter(|| {
-        version_compare2(
-            "1.2.alpha.3.beta.4.pre.5.post.6.patch.7.8a.9b.10c.11.a.12.b.13.c.d.e.f.0",
-            "1.2.alpha.3.beta.4.pre.5.post.6.patch.7.8a.9b.10c.11.a.12.b.13.c.d.e.f.0",
-        )
-    }));
+    c.bench_function("compare mixed", |b| {
+        b.iter(|| {
+            version_compare2(
+                "1.2.alpha.3.beta.4.pre.5.post.6.patch.7.8a.9b.10c.11.a.12.b.13.c.d.e.f.0",
+                "1.2.alpha.3.beta.4.pre.5.post.6.patch.7.8a.9b.10c.11.a.12.b.13.c.d.e.f.0",
+            )
+        })
+    });
 
-    c.bench_function("compare different length", |b| b.iter(|| {
-        version_compare2(
+    c.bench_function("compare different length", |b| {
+        b.iter(|| {
+            version_compare2(
             "1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0",
             "1"
         )
-    }));
+        })
+    });
 }
 
 criterion_group!(benches, comparison_benchmark);
